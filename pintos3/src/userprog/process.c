771c26b946788dbf998b0264740767f28fcd0326
sengcheav <sengcheav@users.noreply.github.com>
2015-05-18 23:47:39 +1200

    Update process.c

--- pintos3/src/userprog/process.c
+++ pintos3/src/userprog/process.c
@@ -102,9 +102,9 @@ static void start_process (void *exec_)
   if (success)
   {   
   	// malloc 
-  	lock_init(&exec_->lock);//#2 
-  	sema_init(&exec_->dead , 0 ) // 0 live 1 dead
-  	&exec.sucess = true ;  // #3
+  	lock_init(&exec_.lock);//#2 
+  	sema_init(&exec_.dead , 0 ) // 0 live 1 dead
+  	&exec->sucess = true ;  // #3
   }
 
   if (!success)
